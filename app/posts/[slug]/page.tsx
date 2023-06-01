ee77efe2f394e28a5fdf45b16a4174ccdbb607ac
Ewelina <ewelinazawol@gmail.com>
2023-06-01 14:54:00 +0100

    Error Handling

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -6,13 +6,13 @@ export const generateStaticParams = async () =>
 
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
-  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
+  if (!post) return { notFound: true };
   return { title: post.title };
 };
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
-  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
+  if (!post) return { notFound: true };
 
   return (
     <div className="mb-8 mx-2 ">
