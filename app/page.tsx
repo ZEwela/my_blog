b3f6c3dec6fd06a8f7e1465a20ecbf8991085418
Ewelina <ewelinazawol@gmail.com>
2023-06-05 10:11:03 +0100

    date

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import Link from "next/link";
 import { compareDesc, format, parseISO } from "date-fns";
 import { allPosts } from "contentlayer/generated";
-export const metadata = {
-  title: "Ewelina Zawol",
-};
+import { useEffect, useState } from "react";
+
+// export const metadata = {
+//   title: "Ewelina Zawol",
+// };
 
 export default function Home() {
   let posts = allPosts.sort(
@@ -12,22 +15,28 @@ export default function Home() {
       b: { date: string | number | Date }
     ) => compareDesc(new Date(a.date), new Date(b.date))
   );
-
   const options: Intl.DateTimeFormatOptions = {
     month: "long",
     day: "numeric",
     year: "numeric",
   };
-  const today = () => {
-    return new Date().toLocaleDateString("en-US", options);
-  };
+  const [date, setDate] = useState(
+    new Date().toLocaleDateString("en-US", options)
+  );
+
+  // const today = () => {
+  //   return new Date().toLocaleDateString("en-US", options);
+  // };
   const fromToday =
-    format(parseISO(posts[0].date), "LLLL d, yyyy") !== today() ? false : true;
+    format(parseISO(posts[0].date), "LLLL d, yyyy") !== date ? false : true;
   let todayPost = null;
   if (fromToday) {
     todayPost = posts[0];
     posts = posts.slice(1);
   }
+  useEffect(() => {
+    setDate(new Date().toLocaleDateString("en-US", options));
+  }, [options]);
 
   return (
     <main className="w-full ">
@@ -35,7 +44,7 @@ export default function Home() {
         <h1 className="mb-8 text-center text-3xl font-black">
           What I have learned today...
         </h1>
-        <p className="text-s text-center ">{today()}</p>
+        <p className="text-s text-center ">{date}</p>
         <div className="mb-8 ">
           {!fromToday ? (
             <div className="mx-auto max-w-2xl py-16 mb-8 text-center text-lg ">
@@ -61,21 +70,19 @@ export default function Home() {
             What I have learned in the past...
           </h2>
           {posts.map((post: any, idx: any) => (
-            <>
-              <div className="flex justify-between my-4 border-b mx-1">
-                <Link
-                  key={idx}
-                  href={post.url}
-                  className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
-                >
-                  <h2 className="mb-1 text-xl">{post.title}</h2>
-                </Link>
+            <div key={idx} className="flex justify-between my-4 border-b mx-1">
+              <Link
+                key={idx}
+                href={post.url}
+                className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
+              >
+                <h2 className="mb-1 text-xl">{post.title}</h2>
+              </Link>
 
-                <time dateTime={post.date} className="mb-2 flex text-s mt-2">
-                  {format(parseISO(post.date), "LLLL d, yyyy")}
-                </time>
-              </div>
-            </>
+              <time dateTime={post.date} className="mb-2 flex text-s mt-2">
+                {format(parseISO(post.date), "LLLL d, yyyy")}
+              </time>
+            </div>
           ))}
         </div>
       </section>
